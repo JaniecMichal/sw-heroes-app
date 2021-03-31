4e39a1cfa62b40672fd449caebc8e976d49d2e03
Michal Janiec <michal.janiec95@gmail.com>
2021-04-01 00:15:33 +0200

    Added userList to rendering

--- src/views/App.js
+++ src/views/App.js
@@ -3,6 +3,7 @@ import { GlobalStyle } from 'assets/GlobalStyles/GlobalStyles.js';
 import { theme } from 'assets/theme/theme.js';
 import { ThemeProvider } from 'styled-components';
 import { MainWrapper } from './styled';
+import UserList from 'components/organisms/UserList';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <>
         <GlobalStyle />
         <MainWrapper>
+          <UserList />
         </MainWrapper>
       </>
     </ThemeProvider>
