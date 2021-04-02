7726d3904b310b1b80775498f099fa35ccccc00f
Michal Janiec <michal.janiec95@gmail.com>
2021-04-03 01:14:38 +0200

    Changed names, added listItems which rendering peopleList from API. Added conditionally rendering if app is in loading state should show spinner instead ListItem

---
+++ src/components/organisms/CharactersList/index.js
@@ -0,0 +1,33 @@
+import React, { useEffect } from 'react';
+import { StyledList, Wrapper } from './styled';
+import ListItem from 'components/molecules/ListItem';
+import { fetchPeople, selectLoading, selectPeople } from 'redux/peopleSlice';
+import { useDispatch, useSelector } from 'react-redux';
+import Spinner from 'components/atoms/Spinner';
+
+const CharactersList = () => {
+  const dispatch = useDispatch();
+  const people = useSelector(selectPeople);
+  const loading = useSelector(selectLoading);
+
+  useEffect(() => {
+    dispatch(fetchPeople());
+  }, [dispatch]);
+
+  return (
+    <Wrapper>
+      <StyledList>
+        {loading ? (
+          <Spinner />
+        ) : (
+          people.length &&
+          people.map((person) => {
+            return <ListItem key={person.name} personData={person} />;
+          })
+        )}
+      </StyledList>
+    </Wrapper>
+  );
+};
+
+export default CharactersList;
--- src/components/organisms/CharactersList/styled.js
+++ src/components/organisms/CharactersList/styled.js
@@ -5,8 +5,8 @@ export const Wrapper = styled.div`
   max-width: 500px;
   box-shadow: -2px 4px 10px rgba(115, 124, 142, 0.09);
   border-radius: 10px;
-  padding: 40px 30px;
-  background-color: ${({ theme }) => theme.colors.white};
+  padding: 10px 30px;
+  background-color: ${({ theme }) => theme.colors.mako};
 `;
 
 export const StyledList = styled.ul`
