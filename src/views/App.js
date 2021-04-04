7a3299aa5a44536606e90232a57d18d6e6a954ea
Michal Janiec <michal.janiec95@gmail.com>
2021-04-05 01:10:38 +0200

    Added Logo component

--- src/views/App.js
+++ src/views/App.js
@@ -4,12 +4,14 @@ import { theme } from 'assets/theme/theme.js';
 import { ThemeProvider } from 'styled-components';
 import { MainWrapper } from './styled';
 import CharactersList from 'components/organisms/CharactersList';
+import Logo from 'components/atoms/Logo';
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
       <>
         <GlobalStyle />
+        <Logo logo={'Star wars heros app'} />
         <MainWrapper>
           <CharactersList />
         </MainWrapper>
