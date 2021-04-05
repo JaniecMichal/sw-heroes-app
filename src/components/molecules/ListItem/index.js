bf8c4e3de216e33c80bdff8dd3b305c55e351081
Michal Janiec <michal.janiec95@gmail.com>
2021-04-06 01:00:05 +0200

    Added routing

--- src/components/molecules/ListItem/index.js
+++ src/components/molecules/ListItem/index.js
@@ -1,28 +1,25 @@
-import React, { useState } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
 import { ItemWrapper } from './styled';
 import CharacterName from 'components/atoms/CharacterName';
 import Button from 'components/atoms/Button';
 import { ReactComponent as ProfileIcon } from 'assets/images/profile.svg';
+import { toPersonDetails } from 'assets/customFunctions/routes';
+import { fetchPersonDetails } from 'redux/peopleSlice';
 
 const UserListItem = ({ personData }) => {
   const { name, gender, birth_year, ...more } = personData;
-  const [showMore, setShowMore] = useState(false);
-  const handleClick = () => {
-    setShowMore(!showMore);
-  };
+  const dispatch = useDispatch();
 
   return (
     <ItemWrapper>
-      <CharacterName
-        name={name}
-        gender={gender}
-        birthYear={birth_year}
-        showMore={showMore}
-        moreDetails={more}
-      />
-      <Button onClick={() => handleClick()}>
-        <ProfileIcon />
+      <CharacterName name={name} gender={gender} birthYear={birth_year} />
+      <Button onClick={() => dispatch(fetchPersonDetails(more.url))}>
+        <Link to={toPersonDetails({ id: name })}>
+          <ProfileIcon />
+        </Link>
       </Button>
     </ItemWrapper>
   );
