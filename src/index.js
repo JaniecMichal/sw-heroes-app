9fca9a0e434ff42d253c1ffa469c6c865f0cfc2c
Michal Janiec <michal.janiec95@gmail.com>
2021-04-06 00:55:53 +0200

    Moved static and common elements of app higher in hierarchy

--- src/index.js
+++ src/index.js
@@ -1,13 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { GlobalStyle } from 'assets/GlobalStyles/GlobalStyles';
+import { theme } from 'assets/theme/theme';
 import { Provider } from 'react-redux';
+import { ThemeProvider } from 'styled-components';
 import App from 'views/App';
 import store from './store';
 
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <ThemeProvider theme={theme}>
+        <GlobalStyle />
+        <App />
+      </ThemeProvider>
     </Provider>
   </React.StrictMode>,
   document.getElementById('root')
