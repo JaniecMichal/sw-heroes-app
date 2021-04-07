eb3d9a72ce2a60acdca955d8e44c27ca72986f4e
Michal Janiec <michal.janiec95@gmail.com>
2021-04-07 02:20:27 +0200

    Moved logic higher

--- src/components/organisms/CharactersList/index.js
+++ src/components/organisms/CharactersList/index.js
@@ -1,36 +1,15 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import { StyledList, Wrapper } from './styled';
 import ListItem from 'components/molecules/ListItem';
-import {
-  fetchFilms,
-  fetchPeople,
-  selectLoading,
-  selectPeople,
-} from 'redux/peopleSlice';
-import { useDispatch, useSelector } from 'react-redux';
-import Spinner from 'components/atoms/Spinner';
 
-const CharactersList = () => {
-  const dispatch = useDispatch();
-  const people = useSelector(selectPeople);
-  const loading = useSelector(selectLoading);
-
-  useEffect(() => {
-    dispatch(fetchPeople());
-    dispatch(fetchFilms());
-  }, [dispatch]);
-
+const CharactersList = ({ people }) => {
   return (
     <Wrapper>
       <StyledList>
-        {loading ? (
-          <Spinner />
-        ) : (
-          people.length &&
+        {people.length &&
           people.map((person) => {
             return <ListItem key={person.name} personData={person} />;
-          })
-        )}
+          })}
       </StyledList>
     </Wrapper>
   );
