7811cd6e23963ba804a0b281210ff8f5afc19a23
Michal Janiec <michal.janiec95@gmail.com>
2021-04-07 02:20:45 +0200

    Set flex direction into column

--- src/views/styled.js
+++ src/views/styled.js
@@ -3,6 +3,7 @@ import styled from 'styled-components';
 export const MainWrapper = styled.main`
   width: 100%;
   display: flex;
+  flex-direction: column;
   justify-content: center;
   align-items: center;
 `;
